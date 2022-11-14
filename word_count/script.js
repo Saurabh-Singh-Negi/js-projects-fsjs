6b1302c587b263b830c57befc4c3ba4dfedf7114
Saurabh <saurabhnegi0007@gmail.com>
2022-11-14 23:19:17 +0530

    html structure & styling added

---
+++ word_count/script.js
@@ -0,0 +1,6 @@
+const word = document.querySelector(".word-item");
+const char = document.querySelector(".char-item");
+const text = document.querySelector(".text-area");
+let word_count = 0;
+let char_count = 0;
+
