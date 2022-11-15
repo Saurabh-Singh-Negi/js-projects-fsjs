2d2d87dd37a3abe54f1e07f4b82db0f6253c8d59
Saurabh <saurabhnegi0007@gmail.com>
2022-11-15 12:05:02 +0530

    word counter solution added

--- word_count/script.js
+++ word_count/script.js
@@ -1,6 +1,16 @@
-const word = document.querySelector(".word-item");
+const word = document.querySelector(".word-text");
 const char = document.querySelector(".char-item");
 const text = document.querySelector(".text-area");
 let word_count = 0;
 let char_count = 0;
+
+
+text.addEventListener("input", () => {
+    char_count = text.value.length;
+    char.innerText = char_count;
+    
+    let word_arr = text.value.split(' ').filter((ele)=>ele !== '');
+    word.innerText = word_arr.length;
+
+})
 
